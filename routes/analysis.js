81c26e08938914365e717f056ce3f37f8b054453
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-01-26 18:15:24 -0800

    add commands check

--- routes/analysis.js
+++ routes/analysis.js
@@ -3,19 +3,28 @@
 var express = require('express');
 var router = express.Router();
 const exec = require('child_process').exec;
+var S = require('string');
 
 router.get('/commands', function(req, res) {
-    exec(req.query.commands, (error, stdout, stderr) => {
-        if (error) {
-            res.json({
-                result: `exec error: ${error}`
-            });
-        } else {
-            res.json({
-                result: `stdout: ${stdout}`
-            });
-        }
-    });
+    let cmd = req.query.commands
+
+    if (S(cmd).startsWith('grep')) {
+        exec(req.query.commands, (error, stdout, stderr) => {
+            if (error) {
+                res.json({
+                    result: `exec error: ${error}`
+                });
+            } else {
+                res.json({
+                    result: `stdout: ${stdout}`
+                });
+            }
+        });
+    } else {
+        res.json({
+            result: 'Not support commands, now only support grep!'
+        })
+    }
 });
 
 module.exports = router;
