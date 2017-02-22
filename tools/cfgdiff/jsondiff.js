6e38440c2cf8eb9719bfdc434c3aaa3b3c8aca56
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-02-21 17:12:46 -0800

    add cli to object

--- tools/cfgdiff/jsondiff.js
+++ tools/cfgdiff/jsondiff.js
@@ -1,29 +1,59 @@
 var diff = require('deep-diff').diff;
+var S = require('string');
+
 const util = require('util');
+const readline = require('readline');
+const fs = require('fs');
+const _ = require('underscore');
 
-// config system global
-//     set admintimeout 480
-//     set alias "FGT60D4615007833"
-//     set fgd-alert-subscription advisory latest-threat
-//     set gui-certificates enable
-//     set hostname "FGT60D4615007833"
-//     set timezone 04
-// end
+const rl = readline.createInterface({
+    input: fs.createReadStream('webfilter.cli')
+});
 
-var lhs = {
-    config: {
-        'system global': {
-            set: {
-                admintimeout: 480,
-                alias: "FGT60D4615007833",
-                'fgd-alert-subscription': ['advisory', 'latest-threat'],
-                'gui-certificates': 'enable',
-                'hostname': "FGT60D4615007833",
-                "timezone": '04',
-            }
-        }
+let cli = {};
+let current = null;
+
+rl.on('line', (line) => {
+    let lines = S(line).trimLeft().parseCSV(' ', null);
+    // console.log(lines);
+    switch (lines[0]) {
+        case 'config':
+            let config_str = S(_.rest(lines)).toCSV(' ', null).s;
+            if (!cli.config) cli.config = {};
+            cli.config[config_str] = {};
+            current = cli.config[config_str];
+            break;
+        case 'edit':
+            let edit_str = S(_.rest(lines)).toCSV(' ', null).s;
+            if (!current.edit) current.edit = {};
+            current.edit[edit_str] = {};
+            current = current.edit[edit_str];
+            break;
+        case 'next':
+            // current = current.parent;
+            break;
+
+        default:
+            // console.log("not support ", lines[0]);
     }
-};
+}).on('close', () => {
+    console.log(util.inspect(cli, { showHidden: true, depth: null }));
+});
+
+// var lhs = {
+//     config: {
+//         'system global': {
+//             set: {
+//                 admintimeout: 480,
+//                 alias: "FGT60D4615007833",
+//                 'fgd-alert-subscription': ['advisory', 'latest-threat'],
+//                 'gui-certificates': 'enable',
+//                 'hostname': "FGT60D4615007833",
+//                 "timezone": '04',
+//             }
+//         }
+//     }
+// };
 
 var rhs = {
     config: {
@@ -40,6 +70,6 @@ var rhs = {
     }
 };
 
-var differences = diff(lhs, rhs);
+// var differences = diff(lhs, rhs);
 
-console.log(util.inspect(differences, { showHidden: true, depth: null }));
+// console.log(util.inspect(differences, { showHidden: true, depth: null }));
