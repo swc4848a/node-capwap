84ef4e9da7861d3e0de91d27d50ab6bf353f569a
zqqiang <qiangzhaoqing@gmail.com>
2017-02-24 17:36:06 -0800

    export jsondiff data to json.js

---
+++ tools/cfgdiff/json.js
@@ -0,0 +1,76 @@
+const rhs = {
+    config: {
+        'webfilter profile': {
+            edit: {
+                default: {
+                    set: {
+                        comment: 'M Default Web Filtering.',
+                        options: [
+                            'activexfilter',
+                            'cookiefilter',
+                            'javafilter',
+                            'block-invalid-url',
+                            'M Add One',
+                            'M Add Two'
+                        ]
+                    },
+                    config: {
+                        override: {
+                            set: {
+                                'ovrd-dur': '1d2h3m',
+                                profile: 'monitor-all'
+                            }
+                        },
+                        web: {
+                            set: {
+                                'bword-table': '1'
+                            }
+                        },
+                        'ftgd-wf': {
+                            set: {
+                                options: ['error-allow',
+                                    'http-err-detail',
+                                    'rate-server-ip',
+                                    'redir-block'
+                                ],
+                                ovrd: ['75', 'g05']
+                            },
+                            config: {
+                                filters: {
+                                    edit: {
+                                        '1': {
+                                            set: {
+                                                category: '2'
+                                            }
+                                        },
+                                        '2': {
+                                            set: {
+                                                category: '7'
+                                            }
+                                        }
+                                    }
+                                },
+                                quota: {
+                                    edit: {
+                                        '1': {
+                                            set: {
+                                                category: 'g02'
+                                            }
+                                        },
+                                        '2': {
+                                            set: {
+                                                duration: '17s'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+module.exports = rhs;
