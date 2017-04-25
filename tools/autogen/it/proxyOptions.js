0e0df9abc8116d797cd0d3d5b6c090f8fa5b0af0
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-04-25 16:28:14 -0700

    add proxy options cases

--- tools/autogen/it/proxyOptions.js
+++ tools/autogen/it/proxyOptions.js
@@ -14,28 +14,45 @@ let cloud_map = {
     'MAPI': "input:checkbox:eq(8)",
     'DNS': "input:checkbox:eq(9)",
     'Comfort Clients': "input:checkbox:eq(10)",
+    'Interval (seconds)': "input:text:eq(90)",
+    'Amount (bytes)': "input:text:eq(91)",
     'Block Oversized File/Email': "input:checkbox:eq(11)",
+    'Threshold (MB)': "input:text:eq(94)",
     'Enable Chunked Bypass': "input:checkbox:eq(12)",
     'Add Fortinet Bar': "input:checkbox:eq(13)",
+    'Communication Port': "input:text:eq(99)",
     'Allow Fragmented Messages': "input:checkbox:eq(14)",
     'Append Signature (SMTP)': "input:checkbox:eq(15)",
+
+    'All Any': "label:contains('Any')",
 
     'Save': "span:contains('Save')",
 }
 
 cases['proxy options edit'] = [];
+cases['proxy options port mapping any'] = [];
+
+let current_case;
 
 let click = (btn) => {
-    cases['proxy options edit'].push([cloud_map[btn], undefined])
+    cases[current_case].push([cloud_map[btn], undefined])
 }
 
 let checked = (btn) => {
     if (cloud_map[btn]) {
-        cases['proxy options edit'].push([cloud_map[btn], true])
+        cases[current_case].push([cloud_map[btn], true])
+    }
+}
+
+let set = (btn, val) => {
+    if (cloud_map[btn]) {
+        cases[current_case].push([cloud_map[btn], val]);
     }
 }
 
 (() => {
+    current_case = 'proxy options edit';
+
     click('Proxy Options');
 
     checked('Log Oversized Files');
@@ -50,17 +67,57 @@ let checked = (btn) => {
     checked('DNS');
 
     checked('Comfort Clients');
+    set('Interval (seconds)', 100);
+    set('Amount (bytes)', 110);
+    
     checked('Block Oversized File/Email');
+    set('Threshold (MB)', 10);
 
     checked('Enable Chunked Bypass');
     checked('Add Fortinet Bar');
+    set('Communication Port', 120)
 
     checked('Allow Fragmented Messages');
     checked('Append Signature (SMTP)');
 
     click('Save');
-})()
+})();
+
+(() => {
+    current_case = 'proxy options port mapping any';
+
+    click('Proxy Options');
+
+    checked('Log Oversized Files');
+    checked('RPC over HTTP');
+    checked('HTTP');
+    checked('SMTP');
+    checked('POP3');
+    checked('IMAP');
+    checked('FTP');
+    checked('NNTP');
+    checked('MAPI');
+    checked('DNS');
+    click('All Any');
+
+    checked('Comfort Clients');
+    set('Interval (seconds)', 100);
+    set('Amount (bytes)', 110);
+    
+    checked('Block Oversized File/Email');
+    set('Threshold (MB)', 10);
+
+    checked('Enable Chunked Bypass');
+    checked('Add Fortinet Bar');
+    set('Communication Port', 120)
+
+    checked('Allow Fragmented Messages');
+    checked('Append Signature (SMTP)');
+
+    click('Save');
+})();
 
 delete cases['proxy options edit'];
+// delete cases['proxy options port mapping any'];
 
 module.exports = cases;
