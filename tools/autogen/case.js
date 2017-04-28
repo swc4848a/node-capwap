e4d66c5f07028730cc46f9bad7fd31f6a35566f7
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-04-28 10:14:50 -0700

    refactor routing testcase

--- tools/autogen/case.js
+++ tools/autogen/case.js
@@ -4,11 +4,11 @@ let cases = require('./it/root.js');
 
 require('./it/interfaces.js');
 require('./it/dns.js');
+require('./it/routing.js');
 
 cases = require('./it/services.js');
 cases = require('./it/schedules.js');
 cases = require('./it/adminSettings.js');
-cases = require('./it/routing.js');
 cases = require('./it/fortiGuard.js');
 cases = require('./it/advanced.js');
 cases = require('./it/addresses.js');
--- tools/autogen/it/routing.js
+++ tools/autogen/it/routing.js
@@ -1,15 +1,51 @@
-let cases = require('./root.js');
+let Testcase = require('../testcase.js');
 
-// todo: bug
-cases['routing new'] = [
-    ["div.gwt-HTML:contains('Routing')"],
-    ["button[title='Create New']"],
-    ["input.gwt-TextBox:eq(0)", "192.168.18.0"],
-    ["input.gwt-TextBox:eq(1)", "255.255.255.0"],
-    ["input.gwt-TextBox:eq(2)", "192.168.1.1"],
-    ["input.gwt-TextBox:eq(3)", 11],
-    ["textarea.gwt-TextArea", "test comments"],
-    ["span:contains('Save')"],
-]
+let map = {
+    'Routing': "div.gwt-HTML:contains('Routing')",
+    'Create New': "button[title='Create New']",
+    'Destination IP': "input.gwt-TextBox:eq(0)",
+    'Destination Netmask': "input.gwt-TextBox:eq(1)",
+    'Gateway': "input.gwt-TextBox:eq(2)",
+    'Administrative Distance': "input.gwt-TextBox:eq(3)",
+    'Device Interface': "select",
+    'Comments': "textarea.gwt-TextArea",
+    'Save': "span:contains('Save')",
 
-module.exports = cases;
+    'Edit Second Item': "div[title='Edit']:eq(1)",
+    'Delete Second Item': "div[title='Delete']:eq(1)",
+    'YES': "span:contains('YES')",
+}
+
+new Testcase('routing new', map, (t) => {
+    t.click('Routing')
+
+    t.click('Create New')
+    t.set('Destination IP', "192.168.18.0")
+    t.set('Destination Netmask', "255.255.255.0")
+    t.set('Gateway', "192.168.1.1")
+    t.set('Administrative Distance', 11)
+    t.set('Device Interface', "internal")
+    t.set('Comments', "test comments")
+
+    t.click('Save')
+})
+
+new Testcase('routing edit', map, (t) => {
+    t.click('Routing')
+    t.click('Edit Second Item')
+
+    t.set('Destination IP', "192.168.18.0")
+    t.set('Destination Netmask', "255.255.255.0")
+    t.set('Gateway', "192.168.1.1")
+    t.set('Administrative Distance', 12)
+    t.set('Device Interface', "wan2")
+    t.set('Comments', "test comments")
+
+    t.click('Save')
+})
+
+new Testcase('routing delete', map, (t) => {
+    t.click('Routing')
+    t.click('Delete Second Item')
+    t.click('YES')
+})
