f58097c38f1bfc8e37ece322593f3e34c8dcad34
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-08-02 17:24:12 -0700

    update chromeless

--- tools/chromeless/index.js
+++ tools/chromeless/index.js
@@ -4,10 +4,11 @@ async function run() {
     const chromeless = new Chromeless()
 
     const screenshot = await chromeless
-        .goto('https://www.google.com')
-        .type('chromeless', 'input[name="q"]')
-        .press(13)
-        .wait('#resultStats')
+        .goto('https://beta.forticloud.com')
+        .type('zqqiang@fortinet.com', 'input#email')
+        .type('SuperCRM801', 'input[type="password"]')
+        .click('input[value="Login"]')
+        .wait(5000)
         .screenshot()
 
     console.log(screenshot) // prints local file path or S3 url
