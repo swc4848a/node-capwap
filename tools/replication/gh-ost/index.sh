e17d992fce5bd3ff5f6a7e5270a771dc8197e352
zqqiang <qiangzhaoqing@gmail.com>
2017-08-25 17:04:05 -0700

    update gh-ost

--- tools/replication/gh-ost/index.sh
+++ tools/replication/gh-ost/index.sh
@@ -1,34 +1,64 @@
 # start server
-mysql-5.7/bin/mysqld --defaults-file=data/s1/s1.cnf --user=root
-mysql-5.7/bin/mysqld --defaults-file=data/s2/s2.cnf --user=root
-mysql-5.7/bin/mysqld --defaults-file=data/s3/s3.cnf --user=root
+mysql-5.7/bin/mysqld --defaults-file=data/s1/s1.cnf --user=root &
+mysql-5.7/bin/mysqld --defaults-file=data/s2/s2.cnf --user=root &
+mysql-5.7/bin/mysqld --defaults-file=data/s3/s3.cnf --user=root &
 
 # connect shell
 mysql-5.7/bin/mysql --port=24801 --host=127.0.0.1
 mysql-5.7/bin/mysql --port=24802 --host=127.0.0.1
 mysql-5.7/bin/mysql --port=24803 --host=127.0.0.1
 
+# sql for master
+SET SQL_LOG_BIN=0;
+CREATE USER rpl_user@'%' IDENTIFIED BY 'rpl_pass';
+GRANT REPLICATION SLAVE ON *.* TO rpl_user@'%';
+FLUSH PRIVILEGES;
+SET SQL_LOG_BIN=1;
+CHANGE MASTER TO MASTER_USER='rpl_user', MASTER_PASSWORD='rpl_pass' FOR CHANNEL 'group_replication_recovery';
+INSTALL PLUGIN group_replication SONAME 'group_replication.so';
+SHOW PLUGINS;
+SET GLOBAL group_replication_bootstrap_group=ON;
+START GROUP_REPLICATION;
+SET GLOBAL group_replication_bootstrap_group=OFF;
+SELECT * FROM performance_schema.replication_group_members;
+
+# sql for slave
+SET SQL_LOG_BIN=0;
+CREATE USER rpl_user@'%';
+GRANT REPLICATION SLAVE ON *.* TO rpl_user@'%' IDENTIFIED BY 'rpl_pass';
+SET SQL_LOG_BIN=1;
+CHANGE MASTER TO MASTER_USER='rpl_user', MASTER_PASSWORD='rpl_pass' FOR CHANNEL 'group_replication_recovery';
+INSTALL PLUGIN group_replication SONAME 'group_replication.so';
+START GROUP_REPLICATION;
+SELECT * FROM performance_schema.replication_group_members;
+
 # gh-ost
-gh-ost \
+./gh-ost \
 --max-load=Threads_running=25 \
 --critical-load=Threads_running=1000 \
 --chunk-size=1000 \
---throttle-control-replicas="127.0.0.1:24801,127.0.0.1:24802,127.0.0.1:24803" \
---max-lag-millis=1500 \
+--throttle-control-replicas="127.0.0.1:24801,127.0.0.1:24803" \
+--max-lag-millis=500 \
+--heartbeat-interval-millis=100 \
 --host=127.0.0.1 \
---port=24802 \
+--port=24803 \
 --database="test" \
 --table="t1" \
 --verbose \
 --alter="engine=innodb" \
 --switch-to-rbr \
---allow-master-master \
+--initially-drop-old-table \
+--initially-drop-ghost-table \
+--initially-drop-socket-file \
+--serve-socket-file=./gh-ost.test.sock \
 --cut-over=default \
 --exact-rowcount \
 --concurrent-rowcount \
---default-retries=120 \
+--default-retries=1 \
 --panic-flag-file=/home/zqqiang/Workspaces/groupReplication/ghost.panic.flag \
---postpone-cut-over-flag-file=/home/zqqiang/Workspaces/groupReplication/ghost.postpone.flag \
---debug \
 --user=root \
+--debug \
+--stack \
 --assume-master-host=127.0.0.1:24801 \
+--execute \
+--alter="change column c2 c3 int not null" --approve-renamed-columns
