670ffdc4b329be27d4cf01b8d06a43cdf1de524a
zqqiang <qiangzhaoqing@gmail.com>
2017-09-28 09:22:57 -0700

    add step form for ssid

--- portal/public/components/ssid.jsx
+++ portal/public/components/ssid.jsx
@@ -2,45 +2,65 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
 
+
+
 @inject('ssidsStore')
 @withRouter
 @observer
 export default class Article extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { step: 1 }
+
+        this.prev = this.prev.bind(this)
+        this.next = this.next.bind(this)
+    }
     componentWillMount() {
         const slug = this.props.match.params.name
         this.props.ssidsStore.loadSSID(slug, { acceptCached: true });
     }
     componentDidMount() {
-        // $("#example-basic").steps({
-        //     headerTag: "h3",
-        //     bodyTag: "section",
-        //     transitionEffect: "slideLeft",
-        //     autoFocus: true
-        // })
+
+    }
+    prev() {
+        this.setState((prevState, props) => ({
+            step: prevState.step - 1
+        }))
+    }
+    next() {
+        this.setState((prevState, props) => ({
+            step: prevState.step + 1
+        }))
     }
     render() {
         const slug = this.props.match.params.name
         const ssid = this.props.ssidsStore.getSSID(slug)
+
+        let step = null
+        switch (this.state.step) {
+            case 1:
+                step = (<div className="box-body">Step1</div>)
+                break
+            case 2:
+                step = (<div className="box-body">Step2</div>)
+                break
+        }
 
         return (
             <section className="content">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="box box-primary">
-                            <div id="example-basic">
-                                <h3>Keyboard</h3>
-                                <section>
-                                    <p>Try the keyboard navigation by clicking arrow left or right!</p>
-                                </section>
-                                <h3>Effects</h3>
-                                <section>
-                                    <p>Wonderful transition effects.</p>
-                                </section>
-                                <h3>Pager</h3>
-                                <section>
-                                    <p>The next and previous buttons help you to navigate through your content.</p>
-                                </section>
+                            <div className="box-header with-border">
+                                <h3 className="box-title">Horizontal Form</h3>
                             </div>
+                            <form className="form-horizontal">
+                                {step}
+                                <div className="box-footer">
+                                    <button className="btn btn-default" onClick={this.prev}>Prev</button>
+                                    <button className="btn btn-info pull-right" onClick={this.next}>Next>></button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
