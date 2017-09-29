4e7d0edab52eeab6e07f0717ebc2508692068c70
zqqiang <qiangzhaoqing@gmail.com>
2017-09-29 09:46:29 -0700

    add ssid step

--- portal/public/components/ssid.jsx
+++ portal/public/components/ssid.jsx
@@ -36,14 +36,22 @@ export default class Article extends React.Component {
         const slug = this.props.match.params.name
         const ssid = this.props.ssidsStore.getSSID(slug)
 
-        let step = null
-        switch (this.state.step) {
-            case 1:
-                step = (<div className="box-body">Step1</div>)
-                break
-            case 2:
-                step = (<div className="box-body">Step2</div>)
-                break
+        function Content({ step }) {
+            return (
+                <div className="step-content body current" 
+                    id="wizard-p-0" 
+                    role="tabpanel" 
+                    aria-labelledby="wizard-h-0" 
+                    aria-hidden="false"
+                >
+                        <div className="text-center m-t-md">
+                            <h2>Hello in Step {step}</h2>
+                            <p>
+                                This is the {step} content.
+                            </p>
+                        </div>
+                </div>
+            )
         }
 
         return (
@@ -52,13 +60,44 @@ export default class Article extends React.Component {
                     <div className="col-lg-12">
                         <div className="box box-primary">
                             <div className="box-header with-border">
-                                <h3 className="box-title">Horizontal Form</h3>
                             </div>
                             <form className="form-horizontal">
-                                {step}
-                                <div className="box-footer">
-                                    <button className="btn btn-default" onClick={this.prev}>Prev</button>
-                                    <button className="btn btn-info pull-right" onClick={this.next}>Next>></button>
+                                <div className="wizard clearfix">
+                                    <div className="steps clearfix">
+                                        <ul role="tablist">
+                                            <li role="tab" className={(1 === this.state.step) ? "first current" : "first disabled"}>
+                                                <a href="javascript:void(0);">
+                                                    {(1 === this.state.step) && <span className="current-info audible">current step: </span>}
+                                                    <span className="">1.</span> First Step
+                                                </a>
+                                            </li>
+                                            <li role="tab" className={(2 === this.state.step) ? "current" : "disabled"}>
+                                                <a href="javascript:void(0);">
+                                                    {(2 === this.state.step) && <span className="current-info audible">current step: </span>}
+                                                    <span className="">2.</span> Second Step
+                                                </a>
+                                            </li>
+                                            <li role="tab" className={(3 === this.state.step) ? "last current" : "last disabled"}>
+                                                <a href="javascript:void(0);">
+                                                    {(3 === this.state.step) && <span className="current-info audible">current step: </span>}
+                                                    <span className="">3.</span> Third Step
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="content clearfix">
+                                        <Content step={this.state.step} />
+                                    </div>
+                                    <div className="actions clearfix">
+                                        <ul role="menu" aria-label="Pagination">
+                                            <li className="disabled" aria-disabled="true">
+                                                <a href="javascript:void(0);" role="menuitem" onClick={this.prev}>Previous</a>
+                                            </li>
+                                            <li aria-hidden="false" aria-disabled="false">
+                                                <a href="javascript:void(0);" role="menuitem" onClick={this.next}>Next</a>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </form>
                         </div>
