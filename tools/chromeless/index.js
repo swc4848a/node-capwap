c15cfcca97a4d365764ada8bdec3391ee17312a3
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-02-23 13:29:32 -0800

    update chromeless

--- tools/chromeless/index.js
+++ tools/chromeless/index.js
@@ -1,11 +1,21 @@
 const { Chromeless } = require('chromeless')
+const chromeLauncher = require('chrome-launcher');
 const path = require('path')
 
 async function run() {
+    var chrome = await chromeLauncher.launch({
+        port: 9222,
+        chromeFlags: [
+            '--start-maximized',
+            '--disable-gpu',
+            // '--headless'
+        ]
+    });
+
     const chromeless = new Chromeless({
         debug: true,
+        launchChrome: false
         // implicitWait: true,
-        // launchChrome: false
     })
 
     const screenshot = await chromeless
@@ -13,12 +23,15 @@ async function run() {
         .type('zqqiang@fortinet.com', 'input[name="username"]')
         .type('SuperCRM801', 'input[name="password"]')
         .click('input[type="submit"]')
-        .wait(20000)
+        .wait(9000)
         .screenshot({ filePath: path.join(__dirname, 'alpha-main.png') })
 
-    console.log(screenshot) // prints local fileÏpath or S3 url
+    //todo: failed to redirect after login
+
+    console.log(screenshot)
 
     await chromeless.end()
+    await chrome.kill()
 }
 
 run().catch(console.error.bind(console))
