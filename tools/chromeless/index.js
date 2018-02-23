e04a83522ee388df792ebc3ee0c8094cf245fbdc
zqqiang <qiangzhaoqing@gmail.com>
2018-02-22 21:18:30 -0800

    update chromeless

--- tools/chromeless/index.js
+++ tools/chromeless/index.js
@@ -1,17 +1,22 @@
 const { Chromeless } = require('chromeless')
+const path = require('path')
 
 async function run() {
-    const chromeless = new Chromeless()
+    const chromeless = new Chromeless({
+        debug: true,
+        implicitWait: true,
+        // launchChrome: false
+    })
 
     const screenshot = await chromeless
-        .goto('https://172.16.95.47')
-        .type('admin', 'input#username')
-        .type('pass', 'input#secretkey')
-        .click('button#login_button')
-        .wait('button:contains("Later")')
-        .screenshot()
+        .goto('https://alpha.forticloud.com')
+        .type('zqqiang@fortinet.com', 'input[name="username"]')
+        .type('SuperCRM801', 'input[name="password"]')
+        .click('input[type="submit"]')
+        .wait(20000)
+        .screenshot({ filePath: path.join(__dirname, 'alpha-main.png') })
 
-    console.log(screenshot)
+    console.log(screenshot) // prints local fileÏpath or S3 url
 
     await chromeless.end()
 }
