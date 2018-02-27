03063aa260f403653096f7004b63ee36f8c7a797
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-02-26 16:42:54 -0800

    udpate config

--- tools/puppeteer/conf/config.js
+++ tools/puppeteer/conf/config.js
@@ -2,7 +2,7 @@ const Config = {
     cloudUrl: `https://alpha.forticloud.com`,
     cloudUsername: `zqqiang@fortinet.com`,
     cloudPassword: `SuperCRM801`,
-    fortigateUrl: `https://172.16.95.49/login`,
+    fortigateUrl: `http://172.16.95.49/login`,
     fortigateUsername: `admin`,
     fortigatePassword: `admin`,
 }
