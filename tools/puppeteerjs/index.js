f7fe1c78223669ac3477d9bfb0eb8be6c1251042
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-03-02 17:17:45 -0800

    clean code

--- tools/puppeteerjs/index.js
+++ tools/puppeteerjs/index.js
@@ -15,6 +15,77 @@ commander.show();
     };
 })();
 
+async function run(page, testcase) {
+    for (const item of testcase.seq) {
+        switch (item.action) {
+            case `screenshot`:
+                await page.screenshot({ path: path.join(__dirname, `/img/${item.filename}`) });
+                break
+            case `evaluate`:
+                await page.evaluate(`${item.script}`)
+                break
+            case `goto`:
+                await page.goto(item.url)
+                break
+            case `type`:
+                if (item.sel.includes(`:`)) {
+                    await page.evaluate(`$("${item.sel}").val("${item.val}")`)
+                } else {
+                    await page.waitFor(item.sel, { timeout: 10000 })
+                    await page.type(item.sel, item.val)
+                }
+                break
+            case `click`:
+                if (item.sel.startsWith(`//`)) {
+                    const elem = await page.waitFor(item.sel, { timeout: 10000 })
+                    await elem.click()
+                } else if (item.sel.includes(`:`)) {
+                    await page.evaluate(`$("${item.sel}").click()`)
+                } else {
+                    await page.click(item.sel)
+                }
+                break
+            case `wait`:
+                await page.waitFor(item.timeout)
+                break
+            case `waitFor`:
+                await page.waitFor(item.sel, { timeout: item.timeout })
+                break
+            case `check`:
+                await page.evaluate(`$("${item.sel}").prop("checked", ${item.val})`)
+                break
+            case `isType`:
+                frame = page.frames().find(frame => frame.name().includes('embedded-iframe'));
+                if (frame) {
+                    result = await frame.$eval(`${item.sel}`, el => el.value)
+                } else {
+                    result = await page.$eval(`${item.sel}`, el => el.value)
+                }
+                console.log(`  result: [${result}] expect: [${item.expect}] => ${result == item.expect ? 'success' : 'failed'}`)
+                break
+            case `isCheck`:
+                frame = page.frames().find(frame => frame.name().includes('embedded-iframe'));
+                if (frame) {
+                    result = await frame.evaluate(`$('${item.sel}').prop("checked")`)
+                } else {
+                    result = await page.evaluate(`$('${item.sel}').prop("checked")`)
+                }
+                console.log(`  result: [${result}] expect: [${item.expect}] => ${result === item.expect ? 'success' : 'failed'}`)
+                break
+            case `isDelete`:
+                result = await page.evaluate(`$('div.first-cell span:contains("${item.target}")').length`);
+                console.log(`  result: [${result}] expect: [${0}] => ${result === 0 ? 'success' : 'failed'}`)
+                break
+            case `has`:
+                result = await page.evaluate(`$(':contains("${item.target}")').length`);
+                console.log(`  result: [${result}] expect: [${1}] => ${result !== 0 ? 'success' : 'failed'}`)
+                break
+            default:
+                console.error(`  unsupport action: ${action}`)
+        }
+    }
+};
+
 (async() => {
     const width = 1600
     const height = 900
@@ -45,74 +116,7 @@ commander.show();
                 continue
             }
             console.log(`  run testcase: ${testcase.name}`)
-            for (const item of testcase.seq) {
-                switch (item.action) {
-                    case `screenshot`:
-                        await page.screenshot({ path: path.join(__dirname, `/img/${item.filename}`) });
-                        break
-                    case `evaluate`:
-                        await page.evaluate(`${item.script}`)
-                        break
-                    case `goto`:
-                        await page.goto(item.url)
-                        break
-                    case `type`:
-                        if (item.sel.includes(`:`)) {
-                            await page.evaluate(`$("${item.sel}").val("${item.val}")`)
-                        } else {
-                            await page.waitFor(item.sel, { timeout: 10000 })
-                            await page.type(item.sel, item.val)
-                        }
-                        break
-                    case `click`:
-                        if (item.sel.startsWith(`//`)) {
-                            const elem = await page.waitFor(item.sel, { timeout: 10000 })
-                            await elem.click()
-                        } else if (item.sel.includes(`:`)) {
-                            await page.evaluate(`$("${item.sel}").click()`)
-                        } else {
-                            await page.click(item.sel)
-                        }
-                        break
-                    case `wait`:
-                        await page.waitFor(item.timeout)
-                        break
-                    case `waitFor`:
-                        await page.waitFor(item.sel, { timeout: item.timeout })
-                        break
-                    case `check`:
-                        await page.evaluate(`$("${item.sel}").prop("checked", ${item.val})`)
-                        break
-                    case `isType`:
-                        frame = page.frames().find(frame => frame.name().includes('embedded-iframe'));
-                        if (frame) {
-                            result = await frame.$eval(`${item.sel}`, el => el.value)
-                        } else {
-                            result = await page.$eval(`${item.sel}`, el => el.value)
-                        }
-                        console.log(`  result: [${result}] expect: [${item.expect}] => ${result == item.expect ? 'success' : 'failed'}`)
-                        break
-                    case `isCheck`:
-                        frame = page.frames().find(frame => frame.name().includes('embedded-iframe'));
-                        if (frame) {
-                            result = await frame.evaluate(`$('${item.sel}').prop("checked")`)
-                        } else {
-                            result = await page.evaluate(`$('${item.sel}').prop("checked")`)
-                        }
-                        console.log(`  result: [${result}] expect: [${item.expect}] => ${result === item.expect ? 'success' : 'failed'}`)
-                        break
-                    case `isDelete`:
-                        result = await page.evaluate(`$('div.first-cell span:contains("${item.target}")').length`);
-                        console.log(`  result: [${result}] expect: [${0}] => ${result === 0 ? 'success' : 'failed'}`)
-                        break
-                    case `has`:
-                        result = await page.evaluate(`$(':contains("${item.target}")').length`);
-                        console.log(`  result: [${result}] expect: [${1}] => ${result !== 0 ? 'success' : 'failed'}`)
-                        break
-                    default:
-                        console.error(`  unsupport action: ${action}`)
-                }
-            }
+            await run(page, testcase)
         }
     } catch (error) {
         console.error(`  catch: `, error)
