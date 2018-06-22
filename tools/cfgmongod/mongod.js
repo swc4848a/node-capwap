b2dd280634684c6d94cb88ec4e6d45cfae569e7d
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-06-22 11:41:11 -0700

    udpate mongo script

--- tools/cfgmongod/mongod.js
+++ tools/cfgmongod/mongod.js
@@ -3,41 +3,87 @@ const Binary = require('mongodb').Binary;
 const fs = require('fs');
 const util = require('util');
 const bytes = require('utf8-length')
+const _ = require('lodash')
 
-const url = `mongodb://172.16.95.48:27017`;
+const url = `mongodb://172.16.95.48:27017,172.16.94.164:27017,172.16.94.163:27017?replicaSet=rs0`;
 const dbname = `cfgserver`;
-const sn = 'FGT60D4615007833';
 
-const insertDocuments = async function (db) {
-    let local = fs.readFileSync(`${sn}/local`);
-    let cacert = fs.readFileSync(`${sn}/cacert`);
-    let config = fs.readFileSync(`${sn}/config.json`);
-    let status = fs.readFileSync(`${sn}/status.json`);
+const collection = [
+    `cacert`, `config`, `dev_status`, `device`, `fulllog`,
+    `local`, `remote`, `remote_1`, `status`, `sysstatus`
+]
 
-    db.collection('fos').insertOne = util.promisify(db.collection('fos').insertOne);
+function randomCollection() {
+    return collection[_.random(collection.length)]
+}
+
+function randomSN() {
+    return (`FGT60D` + _.padStart(`${_.random(10000)}`, 10, '0'))
+}
+
+function isBinary(collection) {
+    return (collection === `cacert` || collection === `dev_status` || collection === `fulllog` ||
+        collection === `local` || collection === `remote` || collection === `remote_1`)
+}
+
+async function insert(db, collection, sn) {
+    let file = fs.readFileSync(isBinary(collection) ? `sample/${collection}` : `sample/${collection}.json`);
+
+    db.collection(`${collection}`).insertOne = util.promisify(db.collection(`${collection}`).insertOne);
 
     let obj = {
         sn: `${sn}`,
-        local: Binary(local),
-        cacert: Binary(cacert),
-        config: JSON.parse(config),
-        status: JSON.parse(status)
+        collection: isBinary(collection) ? Binary(file) : JSON.parse(file),
     }
 
-    await db.collection('fos').insertOne(obj);
+    try {
+        await db.collection(`${collection}`).insertOne(obj);
+    } catch (err) {
+        console.error(err)
+        return 0;
+    }
+
+    console.log(`inserted ${collection} ${sn}`)
 
     return bytes(JSON.stringify(obj))
 }
 
-const insertLimitTest = async function (client) {
+async function update(db, collection, sn) {
+    let hugeFile = fs.readFileSync(isBinary(collection) ? `sample/huge` : `sample/huge.json`);
+
+    db.collection(`${collection}`).update = util.promisify(db.collection(`${collection}`).update);
+
+    try {
+        await db.collection(`${collection}`).update({
+            sn: `${sn}`
+        }, {
+            $inc: {
+                update: 1
+            },
+            $set: {
+                collection: isBinary(collection) ? Binary(hugeFile) : JSON.parse(hugeFile),
+            }
+        });
+    } catch (err) {
+        console.error(err)
+        return 0;
+    }
+
+    console.log(`udpated ${collection} ${sn}`)
+
+    return bytes(JSON.stringify(obj))
+}
+
+async function insertTest(client) {
     const db = client.db(dbname);
-    const max = 100;
+    const max = 10;
     let totalSize = 0;
 
     let start = new Date();
 
     for (let i = 0; i < max; ++i) {
-        totalSize += await insertDocuments(db);
+        totalSize += await insert(db, randomCollection(), randomSN());
+        totalSize += await update(db, randomCollection(), randomSN());
     }
 
     let end = new Date();
@@ -50,13 +96,43 @@ const insertLimitTest = async function (client) {
     console.log(`use ${diff} s, speed ${speed} M/s`);
 }
 
-MongoClient.connect(url, function (err, client) {
-    if (err) {
-        console.error(err);
-        return;
+async function collectionInit(client) {
+    const db = client.db(dbname)
+    db.createCollection = util.promisify(db.createCollection)
+
+    try {
+        await db.createCollection(`cacert`)
+        await db.createCollection(`config`)
+        await db.createCollection(`dev_status`)
+        await db.createCollection(`device`)
+        await db.createCollection(`fulllog`)
+        await db.createCollection(`local`)
+        await db.createCollection(`remote`)
+        await db.createCollection(`remote_1`)
+        await db.createCollection(`status`)
+        await db.createCollection(`sysstatus`)
+    } catch (err) {
+        console.error(err)
     }
 
-    console.log("Connected successfully to server");
+    console.log(`init db collection!`)
+}
 
-    insertLimitTest(client);
-});
+(async () => {
+    let client
+
+    MongoClient.connect = util.promisify(MongoClient.connect)
+
+    try {
+        client = await MongoClient.connect(url)
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.log(`start mongod test client!`)
+
+    await collectionInit(client)
+    await insertTest(client)
+
+    client.close()
+})();
